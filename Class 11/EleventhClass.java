db2c80f1fbfd770505c167c9aee2097c1839bada
EnggAbdulQadir <abdulqadirimran001@gmail.com>
2024-10-04 20:22:21 +0500

    Update

---
+++ Class 11/EleventhClass.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class EleventhClass {
+    public static void main(String[] args) {
+        /// 2D Arrays
+        // Scanner sc = new Scanner(System.in);
+        // int rows = sc.nextInt();
+        // int cols = sc.nextInt();
+        
+        // int[][] numbers = new int[rows][cols];
+
+
+        // // Input (Rows)
+        // for(int i=0; i<rows; i++) {
+        //     // Columns
+        //     for(int j=0; j<cols; j++) {
+        //         numbers[i][j] = sc.nextInt();
+        //     } 
+        // }
+
+        // // Output
+        // for(int i=0; i<rows; i++) {
+        //     for(int j=0; j<cols; j++) {
+        //         System.out.print(numbers[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Practice Question --> Take a matrix as input from the user. Search for a given number x and print the indices at which it occurs.
+        // Scanner sc = new Scanner(System.in);
+        // int rows = sc.nextInt();
+        // int cols = sc.nextInt();
+        
+        // int[][] numbers = new int[rows][cols];
+
+
+        // // Input (Rows)
+        // for(int i=0; i<rows; i++) {
+        //     // Columns
+        //     for(int j=0; j<cols; j++) {
+        //         numbers[i][j] = sc.nextInt();
+        //     } 
+        // }
+
+        // int x = sc.nextInt();
+
+        // for(int i=0; i<rows; i++) {
+        //     for(int j=0; j<cols; j++) {
+        //         // Compare With X
+        //         if(numbers[i][j] == x ) {
+        //             System.out.println("X Found At Location ("+ i +"," + j +")");
+        //         }
+        //     }
+        // }
+
+        // Homework Problems
+        // 1. Print the spiral order matrix as output for a given matrix of numbers. [Difficult for Beginners]
+        // 2. For a given matrix of N x M, print its transpose.
+
+        
+    }
+}
